0f79ed62b3ac4a39e5ab9829f33979b5230138ee
kwamebrowne <theeecryptoking@gmail.com>
2023-06-12 23:28:57 -0500

    idk why tests weren't passig before

--- server/app.py
+++ server/app.py
@@ -4,5 +4,48 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:newroute>')
+def print_string(newroute):
+    print(newroute)
+    return newroute
+
+@app.route('/count/<int:num>')
+def count(num):
+    count = f''
+    for n in range(num):
+        count += f'{n}\n'
+    return count
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, num2, operation):
+    if operation == '*':
+        return str(num1 * num2)
+    
+    elif operation == 'div':
+        return str(num1 / num2)
+    
+    elif operation == '%':
+        return str(num1 % num2)
+    
+    elif operation == '+':
+        return str(num1 + num2)
+    
+    elif operation == '-':
+        return str(num1 - num2)
+    
+    return 'Operation not recognized. Please use one of the following: + - * div %'
+        
+        
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+# Configure app
+#  export FLASK_APP=app.py
+#  export FLASK_RUN_PORT=5555
+#  flask run 
